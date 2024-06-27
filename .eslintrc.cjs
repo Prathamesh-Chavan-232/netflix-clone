4a0bb2ce74bac3d0be86a087152c0d4822e15320
Prathamesh Chavan Noovosoft <prathamesh.chavan@noovosoft.com>
2024-06-27 13:53:34 +0000

    chore: configured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,12 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "project": "./tsconfig.json"
+  },
   plugins: ["react-refresh"],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
